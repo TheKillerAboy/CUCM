e396e04f93bc199ecd9568f534af6374450443db
Annekin Meyburgh <aboy001@ouylook.com>
2019-08-11 21:38:12 +0200

    Update index.js

--- apis/google-sheet/index.js
+++ apis/google-sheet/index.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const path = require('path')
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'apis\\google-sheet\\token.json';
+const TOKEN_PATH = path.join(__dirname,'token.json');
 
 // Load client secrets from a local file.
-fs.readFile('apis\\google-sheet\\credentials.json', (err, content) => {
+fs.readFile(path.join(__dirname,'credentials.json'), (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Sheets API.
     authorize(JSON.parse(content), listMajors);
